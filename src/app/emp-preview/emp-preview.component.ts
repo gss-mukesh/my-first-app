eb9f9baa4994d22326835656995987d718a7275f
Mukesh Jain <mukesh@gamaasoftsolutions.com>
2025-12-07 11:16:43 +0530

    employee component issue fixed

--- src/app/emp-preview/emp-preview.component.ts
+++ src/app/emp-preview/emp-preview.component.ts
@@ -2,22 +2,24 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Employee } from '../employee';
 
 @Component({
-  selector: 'app-emp-pr  ngOnInit(): void {
-    console.log('employee preview here')
-  }
-  ngOnDestroy(): voeview',
+  selector: 'app-emp-preview',
   templateUrl: './emp-preview.component.html',
   styleUrls: ['./emp-preview.component.css']
 })
 export class EmpPreviewComponent implements OnInit, OnDestroy{
 @Input()
 emp1: Employee = new Employee();
-  constructor() { }
 
-id {
-    console.log('component destroy')
+  constructor() { }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
 }
 
 
